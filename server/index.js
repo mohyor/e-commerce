f0c87a504a6ff21615a2e980ed1570c318ef10fe
Leonard Oduneye <chasisdude@gmail.com>
2022-07-03 15:59:16 +0100

    Finished the CRUD

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");/*
+const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
-*/
+
 const cors = require("cors");
 
 dotenv.config();
@@ -26,8 +26,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);/*
+app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
-*/
 
 app.listen(process.env.PORT || 4005, () => { console.log("Backend server is running!") });
